0e7d3acd23c5f785bc4829b0d12b6d0e0e951a59
Asaadlink <asaadfit@gmail.com>
2022-07-13 16:35:28 +0200

    done

---
+++ node-challenge-quote-server/server.js
@@ -0,0 +1,64 @@
+const { response } = require("express");
+// server.js
+// This is where your node app starts
+
+//load the 'express' module which makes writing webservers easy
+const express = require("express");
+const app = express();
+
+//load the quotes JSON
+const quotes = require("./quotes.json");
+
+// Now register handlers for some routes:
+//   /                  - Return some helpful welcome info (text)
+//   /quotes            - Should return all quotes (json)
+//   /quotes/random     - Should return ONE quote (json)
+app.get("/", function (request, response) {
+  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+});
+
+//START OF YOUR CODE...
+
+//Level 1 Challenge - make the quote server//
+
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes));
+});
+
+//Level 2 Challenge - respond from url with parameter//
+
+app.get("/quotes/search", (request, response) => {
+  let term = request.query.term.toLowerCase();
+  
+
+  const filteredObject = quotes.filter((el) => {
+    return (
+      el.quote.toLowerCase().includes(term) ||
+      el.author.toLowerCase().includes(term)
+    );
+  });
+  if (filteredObject.length == 0) {
+    response.status(404).send(filteredObject);
+  }
+  response.send(filteredObject);
+});
+
+//...END OF YOUR CODE
+
+//You can use this function to pick one element at random from a given array
+//example: pickFromArray([1,2,3,4]), or
+//example: pickFromArray(myContactsArray)
+//
+function pickFromArray(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+//Start our server so that it listens for HTTP requests!
+let port = 5000;
+
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
